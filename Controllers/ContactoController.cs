35db39aa37b4cd8d8eebc71d08bd383c72f0f35b
anvorguesa <srliposa@gmail.com>
2021-04-27 01:03:20 -0500

    agregando la parte anterior

---
+++ Controllers/ContactoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CONTACTO_EJERCICIO.Models;
+
+namespace CONTACTO_EJERCICIO.Controllers
+{
+    public class ContactoController : Controller
+    {
+        private readonly ILogger<ContactoController> _logger;
+
+        public ContactoController(ILogger<ContactoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View("Index");
+        }
+    }
+}
